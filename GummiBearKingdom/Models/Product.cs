32e6b9056a4b62cc2080612a43e4ce991b5c8601
Kaili Nishihira <kailinishihira@gmail.com>
2018-03-17 04:08:46 -0700

    Update Product.cs

--- GummiBearKingdom/Models/Product.cs
+++ GummiBearKingdom/Models/Product.cs
@@ -4,22 +4,25 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GummiBearKingdom.Models
-{
-    
+{    
     [Table("Products")]
     public class Product
     {
         [Key]
         public int ProductId { get; set; }
+	    
         [Required]
         public string Name { get; set; }
-		[Required]
+	    
+	[Required]
         public string Description { get; set; }
-		[Required]
+	    
+	[Required]
         public string Cost { get; set; }
-		[Required]
+	    
+	[Required]
         public int CountryId { get; set; }
+	    
         public virtual Country Country { get; set; }
     }
-
 }
